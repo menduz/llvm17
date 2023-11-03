ae6dc97f77af76c8e97d4a09830d0904b7cde31d
menduz <github@menduz.com>
2023-11-03 11:46:53 -0300

    commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Zig compiler development.";
+
+  inputs = {
+    nixpkgs.url = "github:ExpidusOS/nixpkgs/feat/llvm-17";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Used for shell.nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              cmake
+              gdb
+              libxml2
+              ninja
+              qemu
+              wasmtime
+              zlib
+            ]
+            ++ (with llvmPackages_17; [
+              clang
+              clang-unwrapped
+              lld
+              llvm
+            ]);
+
+          hardeningDisable = ["all"];
+        };
+
+        # For compatibility with older versions of the `nix` binary
+        devShell = self.devShells.${system}.default;
+      }
+    );
+}
